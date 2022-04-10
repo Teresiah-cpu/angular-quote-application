1c2b0ad479967f404a4c6563ebfb1c3b7e043b15
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-11 02:55:08 +0300

    Auto-generated commit [ci skip]

--- src/app/count-date.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CountDatePipe } from './count-date.pipe';
-
-describe('CountDatePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CountDatePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
