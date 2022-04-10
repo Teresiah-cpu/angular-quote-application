45299644c5c3174e8f83e684efd8148f430f0112
Teresiah-cpu <terrywambo2018@gmail.com>
2022-04-10 18:44:35 +0300

    add fontawesome icon for votes

--- src/app/votes/votes.component.ts
+++ src/app/votes/votes.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./votes.component.css']
 })
 export class VotesComponent implements OnInit {
+  upvotes:number=0
+  upVotesClick(){
+    this.upvotes ++;
+  }
+  downvotes:number=0
+
+  downVotesClick(){
+    this.downvotes ++;
+  }
 
   constructor() { }
 
